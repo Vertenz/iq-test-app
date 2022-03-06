0436f243bb3d5bd21f512dd11022a3fead27c14e
Vertenz <you@example.com>
2022-03-06 19:29:50 +0300

    create store

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+import MenuComponent from './components/MenuComponent.vue';
 </script>
 
 <template>
-  <router-view></router-view>
+  <header>
+    <header-component />
+    <menu-component />
+  </header>
+  <router-view />
+  <footer>
+    <footer-component />
+  </footer>
 </template>
 
 <style>
--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         <h1>test</h1>
+        <button @click.once="showAbout">show about</button>
     </div>
 </template>
 
 <script setup>
+import { useStore } from 'vuex'
 
+const store = useStore();
+
+const showAbout = () => store.commit('setAnswer', ['showAboutPage', true])
 </script>
 
 <style scoped>
